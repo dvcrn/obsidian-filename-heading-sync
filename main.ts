39163103b18858422152731a2f8d088219f5197c
David Mohl <git@davemail.io>
2021-09-27 20:18:49 +0900

    Revert "Remove file-open event listener"
    This reverts commit 874df629a1c78507b551beb393cb9af25c16dfb5.

--- main.ts
+++ main.ts
@@ -45,6 +45,11 @@ export default class FilenameHeadingSyncPlugin extends Plugin {
     this.registerEvent(
       this.app.vault.on('modify', (file) => this.handleSyncHeadingToFile(file)),
     );
+    this.registerEvent(
+      this.app.workspace.on('file-open', (file) =>
+        this.handleSyncFilenameToHeading(file, file.path),
+      ),
+    );
 
     this.addSettingTab(new FilenameHeadingSyncSettingTab(this.app, this));
 
