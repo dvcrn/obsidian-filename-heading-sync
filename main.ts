2d9c02cbfc397eff62f308e93725938c2d7cd310
David Mohl <git@d.sh>
2025-01-24 13:21:56 +0900

    Run format

--- main.ts
+++ main.ts
@@ -81,7 +81,9 @@ export default class FilenameHeadingSyncPlugin extends Plugin {
       // Register all handlers
       handlers.forEach((h) =>
         this.app.workspace.on(h.event as any, () => {
-          console.log(`[filename-heading-sync] templater event ${h.event} fired, cleaning up`);
+          console.log(
+            `[filename-heading-sync] templater event ${h.event} fired, cleaning up`,
+          );
           h.fn();
         }),
       );
