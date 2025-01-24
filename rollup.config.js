62e1b11f35fbc1bbcc9dcbf3c5767d0e9256f5a1
David Mohl <git@d.sh>
2025-01-24 13:25:43 +0900

    Revert rollup changes

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
   input: 'main.ts',
   output: {
-    dir: '/Users/david/Obsidian/Primary/.obsidian/plugins/obsidian-filename-heading-sync',
+    dir: '.',
     sourcemap: 'inline',
     format: 'cjs',
     exports: 'default'
